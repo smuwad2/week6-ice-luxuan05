af3e428cd623cde13b8e6c01a33e30137874ba5c
luxuan05 <luxuan.wee.2024@computing.smu.edu.sg>
2025-09-24 04:51:11 +0000

    updated ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,23 +3,31 @@
         data() {
             return {
                 // Add code here
-            }
+                id: 'demo1',
+                currentColour: 'redBox', //starting with red
+                buttonClass: 'btn-danger' //button colour 
+            };
         },
         methods: { 
    			// Add code here
+            toggleColour(){
+                this.currentColour = this.currentColour === 'redBox' ? 'blueBox' : 'redBox';
+
+                this.buttonClass = this.buttonClass === 'btn-primary' ? 'btn-danger' : 'btn-primary';
+            }
 		}
-    }
+    };
 </script>
 
 <template>
     
     <!-- Modify code here -->
     <div class="container">
-        <div v-bind:id="id" class="m-2">
+        <div v-bind:id="id" v-bind:class="['m-2', currentColour]">
             div ID : {{id}} 
         </div>
         
-        <button type="button">Change Color</button>
+        <button type="button" class="btn" v-bind:class="buttonClass" v-on:click="toggleColour">Change Color</button>
     </div>
 
 </template>
